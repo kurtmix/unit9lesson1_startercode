7b4c12b6e908707a2229a688c248ba17129fa1cb
kurtmix <kurtmix123@gmail.com>
2016-03-31 20:51:07 +0000

    hey

---
+++ scripts/doom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ 
+    $("button").click(function() {
+      for (i = 0; i < 101 ; i++) { 
+   $("#ed").prepend('<img src="https://optimalotaku.files.wordpress.com/2015/01/10_221cb65b9b1a4be6a2ef043d5eb0b42e3334c2c9_tokyo-ghoul-animc3a911.jpg">');
+}
+    });
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-$(document).onClick(function()
+$(document).ready(function(){
+    $("#check").click(function() {
+       $('#check').fadeOut('fast');
+    });
+});
+$(document).ready(function(){
+    $("#tests").click(function() {
+       alert("Woot woot you got good grades.")
+    });
+});
+$(document).ready(function(){
+    $("#check").click(function() {
+       $('#check').fadeOut('fast');
+    });
+});
